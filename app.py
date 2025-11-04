7675ec09e522c16e93d2ccc1b4bdc5ab5d8fc61c
Manoj-K <k_manoj@apple.com>
2025-11-04 18:15:55 +0530

    corrected the mistakes

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # os.environ['OPENAI_API_KEY']=os.getenv('OPENAI_API_KEY')
-os.environ['LANGCHAIN_API_KEY']=os.getenv('LANGCHAIN_API_KEY')
-os.environ['LANGCHAIN_PROJECT']=os.getenv('LANGCHAIN_PROJECT')
+# os.environ['LANGCHAIN_API_KEY']=os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_PROJECT']='GenAIWithOpenAI'
 os.environ['LANGSMITH_TRACING']="true"
 
 prompt=ChatPromptTemplate.from_messages(
@@ -33,6 +33,9 @@ st.title("MD Chat-Bot")
 
 st.sidebar.title("settings")
 api_key=st.sidebar.text_input("Enter your api key",type="password")
+LANGCHAIN_API_KEY=st.sidebar.text_input("Enter your langchain_key",type="password")
+# LANGCHAIN_PROJECT=st.sidebar.text_input("Enter your langchain_project",type="password")
+
 temperature=st.sidebar.slider("Temparature",0.0,1.0,0.7)
 max_tokens=st.sidebar.slider("Max Token",50,500,100)
 engine=st.sidebar.selectbox("Select engine",["gpt-5","gpt-5-mini",'gpt-5-nano'])
@@ -41,7 +44,7 @@ st.write("Enter your questions")
 user_input=st.text_input('Ask anything you want...')
 
 if user_input and api_key:
-    answer=generate_response(user_input,api_key,engine,temperature,max_tokens)
+    answer=generate_response(user_input,api_key,engine,temperature,max_tokens,LANGCHAIN_API_KEY)
     st.write(answer)
 
 elif user_input:
